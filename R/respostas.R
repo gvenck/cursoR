4cbf52255f38d5f7bd14d316569f19c63e429aea
Fernando Correr <fernando.correr@gmail.com>
2018-06-05 20:53:08 -0300

    Update: session2 and session3 functions

--- R/respostas.R
+++ R/respostas.R
@@ -9,21 +9,21 @@ sessao1 <- function(nome= FALSE, a1=FALSE, b1=FALSE, c1=FALSE, a2 = FALSE, b2 = 
   med_vec <- mean(vec)
   var_vec <- var(vec)
 
-    if(all(a1 == vec)){
-      a1 = TRUE
-      cat("1a ok\n")
-    } else {
-      a1 = FALSE}
-
-    if(b1 == med_vec){
-      b1 = TRUE
-      cat("1b ok\n")
-    } else { b1 = FALSE}
-
-      if(c1 == var_vec){
-      c1 = TRUE
-      cat("1c ok\n")
-    } else { c1 = FALSE}
+  if(all(a1 == vec)){
+    a1 = TRUE
+    cat("1a ok\n")
+  } else {
+    a1 = FALSE}
+  
+  if(b1 == med_vec){
+    b1 = TRUE
+    cat("1b ok\n")
+  } else { b1 = FALSE}
+  
+  if(c1 == var_vec){
+    c1 = TRUE
+    cat("1c ok\n")
+  } else { c1 = FALSE}
 
   ## Ex 2
 
@@ -35,27 +35,26 @@ sessao1 <- function(nome= FALSE, a1=FALSE, b1=FALSE, c1=FALSE, a2 = FALSE, b2 = 
   var_IMC <- var(IMC)
   sd_IMC <- sd(IMC)
 
-    if(all(a2 == IMC)){
-      a2 = TRUE
-      cat("2a ok\n")
-    } else {
-      a2 = FALSE}
-
-      if(b2 == md_IMC){
-      b2 = TRUE
-      cat("2b ok\n")
-    } else { b2 = FALSE}
-
-
-    if(c2 == var_IMC){
-      c2 = TRUE
-      cat("2c ok\n")
-    } else { c2 = FALSE}
-
-      if(d2 == sd_IMC){
-      d2 = TRUE
-      cat("2d ok\n")
-    } else { d2 = FALSE}
+  if(all(a2 == IMC)){
+    a2 = TRUE
+    cat("2a ok\n")
+  } else {
+    a2 = FALSE}
+  
+  if(b2 == md_IMC){
+    b2 = TRUE
+    cat("2b ok\n")
+  } else { b2 = FALSE}
+  
+  if(c2 == var_IMC){
+    c2 = TRUE
+    cat("2c ok\n")
+  } else { c2 = FALSE}
+  
+  if(d2 == sd_IMC){
+    d2 = TRUE
+    cat("2d ok\n")
+  } else { d2 = FALSE}
 
   ## Ex 3
 
@@ -65,10 +64,10 @@ sessao1 <- function(nome= FALSE, a1=FALSE, b1=FALSE, c1=FALSE, a2 = FALSE, b2 = 
   x <- c(x1,x2)
 
 
-    if(all(a3 == x)){
-      a3 = TRUE
-      cat("a3 ok\n")
-    } else { a3 = FALSE}
+  if(all(a3 == x)){
+    a3 = TRUE
+    cat("a3 ok\n")
+  } else { a3 = FALSE}
 
   resp <- c(nome, a1,b1,c1, a2,b2,c2,d2,a3)
 
@@ -77,7 +76,95 @@ sessao1 <- function(nome= FALSE, a1=FALSE, b1=FALSE, c1=FALSE, a2 = FALSE, b2 = 
   post_answers(post_content= resp)
 }
 
+sessao2 <- function(nome= FALSE, a1=FALSE, b1=FALSE, c1=FALSE, 
+                    a2 = FALSE, b2 = FALSE, 
+                    a3 = FALSE, b3 = FALSE, c3 = FALSE){
+  ## Ex 1
+  attach(mtcars)
+  cl_mt <- class(mtcars)
+  cl_cyl_hp <- c(class(mtcars$cyl),class(mtcars$hp))
+  cl_rnames <- class(rownames(mtcars))
+  
+  if(a1 == cl_mt){
+    a1 = TRUE
+    cat("1a ok\n")
+  } else {
+    a1 = FALSE}
+  
+  if(all(b1 == cl_cyl_hp)){
+    b1 = TRUE
+    cat("1b ok\n")
+  } else { b1 = FALSE}
+  
+  if(c1 == cl_rnames){
+    c1 = TRUE
+    cat("1c ok\n")
+  } else { c1 = FALSE}
+  
+  ## Ex 2
+  hp_mean <- mean(mtcars$hp)
+  hp_fac_mean <- mean(as.factor(mtcars$hp)) #Resulta em NA
+  
+  if(a2 == hp_mean){
+    a2 = TRUE
+    cat("2a ok\n")
+  } else {
+    a2 = FALSE}
+  
+  if(is.na(b2)){ #Confere se é NA
+    b2 = TRUE
+    cat("2b ok\n")
+  } else { b2 = FALSE}
+  
+  ##Ex 3
+  list_mam <- list(Nomes= c("Dinho", "Bento", "Samuel", "Sérgio", "Rasec"),
+                   Função=c("Vocalista","Guitarrista", "Baixista", "Baterista","Tecladista"),
+                   Idade = c(24, 25, 22, 26, 28))
+  l_idade <- mean(list_mam$Idade)
+  l_ordered <- lapply(list_mam, function(x){ x[order(list_mam$Nomes)]})
+
+  if(all.equal(a3, list_mam)){
+    a3 = TRUE
+    cat("a3 ok\n")
+  } else { a3 = FALSE}
+  
+  if(b3 == l_idade){
+    b3 = TRUE
+    cat("b3 ok\n")
+  } else { b3 = FALSE}
+  
+  if(all.equal(c3, l_ordered)){
+    c3 = TRUE
+    cat("c3 ok\n")
+  } else { c3 = FALSE}
+}
+
+sessao3 <- function(nome= FALSE, a1=FALSE, b1=FALSE, c1=FALSE){
+  
+  ## Ex 1
+  set.seed(123456)
+  rn_val <- rnorm(n=3000, mean = 20, sd = 5)
+  rn_soma <- sum(rn_val[rn_val>30])
+  rn_soma_sel <- sum(rn_val[rn_val<mean(rn_val)])
+  rn_rem_min <- rn_val[rn_val > min(rn_val)]
+
+  if(a1 == rn_soma){
+    a1 = TRUE
+    cat("1a ok\n")
+  } else {
+    a1 = FALSE}
+  
+  if(b1 == rn_soma_sel){
+    b1 = TRUE
+    cat("1b ok\n")
+  } else { b1 = FALSE}
+  
+  if(all(c1 == rn_rem_min)){
+    c1 = TRUE
+    cat("1c ok\n")
+  } else { c1 = FALSE}
+}
+
 ##'@export
 
 
-sessao1(b1 = med_vec)
